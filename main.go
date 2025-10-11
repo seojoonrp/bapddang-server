2ac77e6138e52f041cccd26ee8adaa0289dab8b6
seojoonrp <seojoonrp@snu.ac.kr>
2025-10-12 02:08:39 +0900

    docs: File structure

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/seojoonrp/bapddang-server/handlers"
+	"github.com/seojoonrp/bapddang-server/api/handlers"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
